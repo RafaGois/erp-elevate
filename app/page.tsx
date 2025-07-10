05fb34bd89bfca178583e223616c34236bd0aa27
rafael.gois <api.mendes@mendesmaquinas.com.br>
2025-07-10 17:45:53 -0300

    fix: seila

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
       <h1>Hello World</h1>
+      <Link href="/auth" >
+      <Button variant="outline">Entrar</Button>
+      </Link>
     </div>
   );
 }
