5d64163790578fc9ba09e4ac67061763b32df548
rafael.gois <api.mendes@mendesmaquinas.com.br>
2025-07-10 14:10:26 -0300

    dskfndjbghfdl

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-import { Button } from "../../components/ui/button";
-import { Card, CardContent } from "../../components/ui/card";
-import { Input } from "../../components/ui/input";
-import { Label } from "../../components/ui/label";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import Image from "next/image";
 
-import useAuth from "../../data/hooks/useAuth";
-import {User} from "../../lib/models/User";
+import useAuth from "@/data/hooks/useAuth";
+import { User } from "@/lib/models/User";
 import { Form } from "@/components/ui/form";
 import { useForm } from "react-hook-form";
 
