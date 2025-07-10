3ecd36c49e4a6170e2d7dbeeda95b0ed7327ab94
rafael.gois <api.mendes@mendesmaquinas.com.br>
2025-07-10 14:34:13 -0300

    fix: correcao git (git config core.ignorecase false)

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import * as service from '@/lib/services/user.service';
 import { HttpError } from 'http-errors';
-import {User} from '@/lib/models/User';
+import { User } from '@/lib/models/User';
 import { handleError } from '@/lib/utils/withErrorHandler';
 
 
