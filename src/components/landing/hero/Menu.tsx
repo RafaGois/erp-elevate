0959fe1f7266bbd6449596f2bdf3f393d5584823
rafael.gois <api.mendes@mendesmaquinas.com.br>
2025-07-16 16:54:16 -0300

    fix: novas mobile navbar

--- src/components/landing/hero/Menu.tsx
+++ src/components/landing/hero/Menu.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { useEffect, useRef, useState } from "react";
 import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
+import MenuItem from "./MenuItem";
 
 const menuLinks = [
   {
@@ -32,23 +33,28 @@ export default function Menu() {
 
   useGSAP(
     () => {
-      gsap.set(".menu-item-link-item-holder", { y: 75 });
+      gsap.set(".menu-item-link-item-holder", { xPercent: 100, opacity: 0 });
 
       tl.current = gsap
         .timeline({ paused: true })
         .to(".menu-overlay", {
           display: "flex",
-          duration: 1.25,
+          duration: 0.7,
+          clipPath: "polygon(0% 100%, 0% 0%, 100% 100%, 0% 100%)",
+          ease: "power4.inOut",
+        })
+        .to(".menu-overlay", {
           clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
           ease: "power4.inOut",
         })
         .to(".menu-item-link-item-holder", {
-          y: 0,
+          xPercent: 0,
           duration: 1,
           stagger: 0.1,
           ease: "power4.inOut",
           delay: -0.75,
-        });
+          opacity: 1,
+        }, "+=0.1");
     },
     { scope: container }
   );
@@ -65,52 +71,52 @@ export default function Menu() {
     setIsOpen(!isOpen);
   }
 
-    return (
-      <div ref={container} className="justify-between flex w-full">
-        <div>logo</div>
-        <div className="flex-row gap-[2rem] hidden sm:flex">
-          <a href="">Sobre nós</a>
-          <a href="">Trabalhos</a>
-          <a href="">Clinetes</a>
-          <a href="#contact">Contato</a>
-        </div>
-        <Button variant="outline" className="hidden sm:flex">
-          <Link href="/auth">Entrar</Link>
-        </Button>
-        <div className="flex sm:hidden w-full justify-end">
-          <MenuIcon
-            className="cursor-pointer"
-            onClick={toggleMenu}
-          />
-        </div>
-        <div
-          className={`hidden menu-overlay fixed top-0 left-0 w-lvw h-lvh bg-[#bdfa3c] z-50 flex-col items-center justify-center p-4`}
-        >
-          <div className="flex w-full justify-end">
-            <X className="cursor-pointer" onClick={toggleMenu} />
-          </div>
-          <div className="flex flex-col justify-center gap-4 flex-1">
-            {menuLinks.map((link) => (
-              <div key={link.href} className="menu-item-link-item">
-                <div key={link.href} className="menu-item-link-item-holder" onClick={toggleMenu}>
-                  <Link
-                    className="menu-link  text-5xl font-bold"
-                    key={link.href}
-                    href={link.href}
-                  >
-                    {link.label}
-                  </Link>
-                </div>
-              </div>
-            ))}
-          </div>
-          <div className="flex w-full justify-end">
-            <Button variant="outline">
-              <Link href="/auth">Entrar</Link>
-            </Button>
-          </div>
-        </div>
-      </div>
-    );
+  function handleMouseEnter(e: React.MouseEvent<HTMLAnchorElement>) {
+    gsap.to(e.target, {
+      scale: 1.1,
+      duration: 0.5,
+      ease: "power4.inOut",
+    });
   }
 
+  return (
+    <div ref={container} className="justify-between flex w-full">
+      <div>logo</div>
+      <div className="flex-row gap-[2rem] hidden sm:flex">
+        <a
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={() => null}
+          href=""
+        >
+          Sobre nós
+        </a>
+        <a href="">Trabalhos</a>
+        <a href="">Clinetes</a>
+        <a href="#contact">Contato</a>
+      </div>
+      <Button variant="outline" className="hidden sm:flex">
+        <Link href="/auth">Entrar</Link>
+      </Button>
+      <div className="flex sm:hidden w-full justify-end">
+        <MenuIcon className="cursor-pointer" onClick={toggleMenu} />
+      </div>
+      <div
+        className={`hidden menu-overlay fixed top-0 left-0 w-lvw h-lvh /bg-[#bdfa3c] bg-black z-50 flex-col items-center justify-center p-4`}
+      >
+        <div className="flex w-full justify-end">
+          <X className="cursor-pointer" onClick={toggleMenu} />
+        </div>
+        <div className="flex flex-col justify-center gap-4 flex-1">
+          {menuLinks.map((link) => (
+            <MenuItem key={link.href} link={link} toggleMenu={toggleMenu} />
+          ))}
+        </div>
+        <div className="flex w-full justify-end">
+          <Button variant="outline">
+            <Link href="/auth">Entrar</Link>
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/landing/hero/MenuItem.tsx
@@ -0,0 +1,63 @@
+import Link from "next/link";
+import { gsap } from "gsap";
+
+export default function MenuItem(props: {
+  link: { href: string; label: string };
+  toggleMenu: () => void;
+}) {
+  function handleMouseEnter(e: React.MouseEvent<HTMLDivElement>) {
+    const tl = gsap.timeline();
+
+    tl.to(e.currentTarget.querySelector(".menu-link"), {
+      color: "#bdfa3c",
+      scale: 1.1,
+      duration: 0.5,
+    }).to(
+      e.currentTarget.querySelector(".dot"),
+      {
+        width: "100%",
+        height: 5,
+        backgroundColor: "#bdfa3c",
+      },
+      "<"
+    );
+  }
+
+  function handleMouseLeave(e: React.MouseEvent<HTMLDivElement>) {
+    const tl = gsap.timeline();
+
+    tl.to(e.currentTarget.querySelector(".menu-link"), {
+      color: "#dadada",
+      scale: 1,
+      duration: 0.5,
+    }).to(
+      e.currentTarget.querySelector(".dot"),
+      {
+        width: 0,
+        height: 0,
+      },
+      "<"
+    );
+  }
+
+  return (
+    <div key={props.link.href} className="menu-item-link-item">
+      <div
+        key={props.link.href}
+        className="menu-item-link-item-holder"
+        onClick={props.toggleMenu}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        <Link
+          className="menu-link  text-5xl font-bold"
+          key={props.link.href}
+          href={props.link.href}
+        >
+          {props.link.label}
+        </Link>
+        <div className="dot"></div>
+      </div>
+    </div>
+  );
+}
