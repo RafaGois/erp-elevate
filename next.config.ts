a13cb75c2e306c8b9d0505d70846b524d6945371
rafael.gois <api.mendes@mendesmaquinas.com.br>
2025-07-25 14:50:46 -0300

    fix: headers

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    async headers() {
+        return [
+            {
+                source: "/auth",
+                headers: [
+                    {
+                        key: "Cross-Origin-Embedder-Policy",
+                        value: "unsafe-none",
+                    },
+                    {
+                        key: "Cross-Origin-Opener-Policy",
+                        value: "unsafe-none",
+                    },
+                ]
+            }
+        ]
+    }
 };
 
 export default nextConfig;
