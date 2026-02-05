c71bf8e9faee6139a3e981b15d5e65861dae3dd4
RafaGois <rafaelfontanabg@gmail.com>
2026-02-04 22:25:22 -0300

    fix: removing breadcumb

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,19 +1,15 @@
 "use client";
 
 import { SidebarTrigger } from "../ui/sidebar";
-import { usePathname } from "next/navigation";
-import AppBreadcrumb from "../default/AppBreadcrumb";
 import { Menu } from "lucide-react";
 
 export default function Header() {
-  const pathname = usePathname();
   
   return (
     <div className="flex p-5 pr-8 border-b" style={{ gridArea: "cabecalho" }}>
       <SidebarTrigger  className="mr-3">
         <Menu />
       </SidebarTrigger>
-      <AppBreadcrumb route={pathname || ""} />
     </div>
   );
 }
